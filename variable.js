8c6118d641779eb6dd1b7a20b669e5e0816b6fff
Anand Dubey <anand876770@gmail.com>
2026-05-31 20:41:32 +0530

    Add

---
+++ variable.js
@@ -0,0 +1,14 @@
+const accountId = 6262172262
+let accountEmail = "aman@google.com"
+var accountPassword = "123456"
+accountCity = "New Delhi"
+let accountState;
+
+//accountId = 2 //NOt Allowed 
+accountEmail = "hc@hc.com"
+accountPassword = "5222552"
+accountCity = "Noida"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountCity, accountState])
